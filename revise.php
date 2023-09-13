23cbd20ec94beaa74a05c53d3c7a32b819848914
hamzabilalaydin <hamzaba61@gmail.com>
2023-09-13 21:54:52 +0300

    yeni dosyalar eklendi

---
+++ revise.php
@@ -0,0 +1,56 @@
+<?php
+require 'login.kontrol.php';
+require 'sayfa.ust.php';
+require_once('db.php');
+
+$user_id = $_SESSION['id'];
+$book_id = $_GET['id'];
+
+$my_sql = "SELECT review FROM review WHERE kullanici_id = :user_id AND books_id = :book_id";
+$my_sorgu = $DB->prepare($my_sql);
+$my_sorgu->bindParam(':user_id', $user_id);
+$my_sorgu->bindParam(':book_id', $book_id);
+$my_sorgu->execute();
+$my_review = $my_sorgu->fetchAll(PDO::FETCH_ASSOC);
+$old_review = $my_review[0]['review'];
+
+if(isset($_POST['submit_form'])){
+    
+    $user_id = $_SESSION['id'];
+    $review = $_POST['submit_form'];
+    $book_id = $_GET['id'];
+
+    $sql = 'UPDATE review SET review = :review WHERE kullanici_id = :user_id AND books_id = :book_id';
+    $SORGU = $DB->prepare($sql);
+    
+    $SORGU->bindParam(':user_id', $user_id);
+    $SORGU->bindParam(':book_id', $book_id);
+    $SORGU->bindParam(':review', $review);
+    
+    $SORGU->execute();
+
+    header("location: login.php");
+    die();
+}
+#$kullanicilar = $SORGU->fetchAll(PDO::FETCH_ASSOC);
+
+echo "
+<br><br>
+<div class='alert text-center' style='background-color: #FF9F9F' role='alert'>
+<b> YOUR COMMENT </b>
+</div>
+
+<form method='POST'>
+<div class='form-floating'>
+<textarea class='form-control' name='submit_form' id='submit_form' style='background-color: #DAC0A3; height:300px;' placeholder='Leave a comment here' id='floatingTextarea2' style='height: 100px'>$old_review</textarea>
+<label for='floatingTextarea2'><a style='color: #0F2C59;'>Your Comment</a></label>
+</div>
+<div class='d-grid mt-3 mb-1 gap-2'>
+<input class='btn text-white' href='index.php' style='background-color: #0F2C59;' type='submit' value='Submit'>
+</div>
+</form>
+
+";
+    
+
+require_once 'sayfa.alt.php'; ?>
---
+++ submit_review.php
@@ -0,0 +1,46 @@
+<?php
+require 'login.kontrol.php';
+require 'sayfa.ust.php';
+require_once('db.php');
+
+
+if(isset($_POST['submit_form'])){
+    
+    $review = $_POST['submit_form'];
+    $user_id = $_SESSION['id'];
+    $book_id = $_GET['id'];
+    
+    $sql = 'INSERT INTO review (kullanici_id, books_id, review) VALUES (:user_id, :book_id, :review)';
+    $SORGU = $DB->prepare($sql);
+    
+    $SORGU->bindParam(':user_id', $user_id);
+    $SORGU->bindParam(':book_id', $book_id);
+    $SORGU->bindParam(':review', $review);
+    
+    $SORGU->execute();
+
+    header("location: login.php");
+    die();
+}
+#$kullanicilar = $SORGU->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+<br><br>
+<div class='alert text-center' style='background-color: #FF9F9F' role='alert'>
+    <b> YOUR COMMENT </b>
+</div>
+
+<form method='POST'">
+    <div class='form-floating'>
+        <textarea class='form-control' name='submit_form' id='submit_form' style='background-color: #DAC0A3; height:300px;' placeholder='Leave a comment here' id='floatingTextarea2' style='height: 100px'></textarea>
+        <label for='floatingTextarea2'><a style='color: #0F2C59;'>Your Comment</a></label>
+    </div>
+    <div class='d-grid mt-3 mb-1 gap-2'>
+        <input class='btn text-white' href='index.php' style='background-color: #0F2C59;' type='submit' value='Submit'>
+    </div>
+</form>
+
+
+
+<?php require_once 'sayfa.alt.php'; ?>
